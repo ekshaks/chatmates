4981d0eb984d2c9cf80ddd4ff36c246e6cd1d54b
Nishant Sinha <nishant@offnote.co>
2023-06-01 15:42:27 +0530

    add field,refactor

--- chatmates/app/doc_ask.py
+++ chatmates/app/doc_ask.py
@@ -1,37 +1,27 @@
 
 from typing import List, Optional
 
-from docarray.typing import NdArray
-from docarray import BaseDoc, DocList
-from pydantic import Field
 
+from docarray import BaseDoc, DocList
 from docarray.index import HnswDocumentIndex as HI
 
 from chatmates.embed import HuggingFaceEmbeddings, HuggingFaceInstructEmbeddings
 
 from chatmates.common import log
 
-class IDoc(BaseDoc):
-    text: str  # concatenation of title, overview and actors names
-    dense_rep: Optional[NdArray[768]] = Field(
-        is_embedding=True
-    )  # vector representation of the document
-
-    def make_rep(self, embed_fn):
-        self.dense_rep = embed_fn(self.text)
-
-    @staticmethod
-    def from_part(part):
-        return IDoc(text=part['text'], dense_rep=None)
 
 class DocAsk:
-    def __init__(self, index_path='./idoc_index/default') -> None:
+    def __init__(self, IDoc, index_path='./idoc_index/default') -> None:
+        '''
+        @IDoc : BaseDoc instance to be stored
+        '''
         super().__init__()
         self.encoder = None
         self.embed_dim = 768
         self.idocs: List[IDoc] = []
 
         self.index = HI[IDoc](work_dir=index_path)
+        self.IDoc = IDoc
 
     
     def _embed_text(self, text):
@@ -45,29 +35,30 @@ class DocAsk:
         self.doc = json_doc #simple list of docs (f1 | f2 | ..)*
 
         log('build idoc')
-        self.idocs = [IDoc.from_part(part) for part in self.doc]
+        self.idocs = [self.IDoc.from_part(part) for part in self.doc]
         log('embedding')
         for doc in self.idocs:
             doc.make_rep(self._embed_text)
 
         log('indexing')
-        dl = DocList[IDoc](self.idocs)
+        dl = DocList[self.IDoc](self.idocs)
         self.dl = dl
         self.index.index(dl)
         
-    def query(self, query_text, limit=10):
+    def query(self, query_text, search_field, limit=10, out_fields=None):
         qrep = self._embed_text(query_text)
         store = self.index
         #vector_q = store.build_query().find(qrep).limit(10).build()
         #vector_results = store.execute_query(vector_q)
-        results, scores = store.find(qrep, limit=limit, search_field='dense_rep')
-        results_text = [res.text for res in results]
-        res_scores = list(zip(results_text, scores))
+        results, scores = store.find(qrep, limit=limit, search_field=search_field)
+
+        results = [ {key: getattr(res, key) for key in out_fields}
+                for res in results]
+        #results_text = [res.text for res in results]
+        res_scores = list(zip(results, scores))
         for res, score in res_scores:
             print(score, res)
 
-            
-        
         return res_scores
 
     
--- tests/tdoc_ask.py
+++ tests/tdoc_ask.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(0,'..')
 
+from typing import List, Optional
+
 
 from dataclasses import dataclass
 import click
@@ -9,7 +11,26 @@ from chatmates import app
 from chatmates.app.doc_ask import DocAsk
 
 from chatmates.mind_tasks import answer_query_from_context
+import random
 
+from docarray.typing import NdArray
+from docarray import BaseDoc
+from pydantic import Field
+
+class IDoc(BaseDoc):
+    text: str  # concatenation of title, overview and actors names
+    dense_rep: Optional[NdArray[768]] = Field(
+        is_embedding=True
+    )  # vector representation of the document
+    section: Optional[str] = ''
+
+    def make_rep(self, embed_fn):
+        self.dense_rep = embed_fn(self.text)
+
+    @staticmethod
+    def from_part(part):
+        return IDoc(text=part['text'], dense_rep=None, section=part['section'])
+    
 @click.command()
 @click.option("--doc", '-d', help="Text File")
 @click.option("--build_index", '-b', is_flag=True, help="build index")
@@ -17,29 +38,24 @@ from chatmates.mind_tasks import answer_query_from_context
 @click.option("--model", '-m', help="model id")
 def ask_doc(doc, build_index=False, query=None, model='7B'):
 
-    DA = DocAsk()
+    DA = DocAsk(IDoc)
 
     if build_index:
-        if doc is None:
-            assert False
-            doc = [
-                dict(text='a. All employees are eligible to apply for remote work arrangements, subject to their job requirements and departmental approval.'),
-                dict(text="b. Remote work may be granted on a full-time or part-time basis, depending on the nature of the job and employee's performance."),
-                dict(text="Managers should communicate the decision to the employee within [X] days of receiving the request.")
-            ]
-        else:
-            with open(doc, 'r') as fp:
-                lines = fp.readlines()
-                doc = [dict(text=line) for line in lines if line.strip()] 
+        assert doc is not None
+        with open(doc, 'r') as fp:
+            lines = fp.readlines()
+            #doc = [dict(text=line) for line in lines if line.strip()] 
+            doc = [dict(text=line, section=str(random.randint(0,100)) ) for line in lines if line.strip()] 
 
 
     if build_index:
         DA.ingest_embed_index(doc)
 
     if query: 
-        res_scores: '(text, score)*' = DA.query(query)
-        response = answer_query_from_context(query, res_scores, model_id=model)
-        print(response)
+        res_scores: '(text, score)*' = DA.query(query, search_field='dense_rep', 
+                                                out_fields=['text', 'section'])
+        #response = answer_query_from_context(query, res_scores, model_id=model)
+        #print(response)
 
     
 @click.group()
